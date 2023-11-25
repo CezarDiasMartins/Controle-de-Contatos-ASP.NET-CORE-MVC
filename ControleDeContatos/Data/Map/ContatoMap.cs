824576a34957ad09ecbf522514644256a080e7d5
CezarDiasMartins <cezardiasmartins06@hotmail.com>
2023-11-25 17:54:56 -0400

    primeiro commit

---
+++ ControleDeContatos/Data/Map/ContatoMap.cs
@@ -0,0 +1,16 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ControleDeContatos.Data.Map
+{
+    // Mapeamento do relacionamento da ContatoModel
+    public class ContatoMap : IEntityTypeConfiguration<ContatoModel>
+    {
+        public void Configure(EntityTypeBuilder<ContatoModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Usuario);
+        }
+    }
+}
